db2893f86c8e4334d028768686b3ffa3af73c3ae
NahuelAB <17nahuel1717@gmail.com>
2023-01-25 20:44:57 -0300

    Header

--- ProyectoAP/src/app/app.module.ts
+++ ProyectoAP/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesSocialesComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
---
+++ ProyectoAP/src/app/components/redes-sociales/redes-sociales.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redes-sociales',
+  templateUrl: './redes-sociales.component.html',
+  styleUrls: ['./redes-sociales.component.css']
+})
+export class RedesSocialesComponent {
+
+}
