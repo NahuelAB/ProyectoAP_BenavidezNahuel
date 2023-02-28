8b3b7199bafec230409882be6a0ec6070f567677
NahuelAB <17nahuel1717@gmail.com>
2023-02-28 19:12:15 -0300

    Creacion Carpeta BackEnd

---
+++ ProyectoAP/BackEnd/src/main/java/com/portfolio/nahuelb/NahuelbApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.nahuelb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NahuelbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NahuelbApplication.class, args);
+	}
+
+}
